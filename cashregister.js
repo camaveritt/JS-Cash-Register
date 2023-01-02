43fedeb508670723e73af524a13c563d5927f35e
Cameron <camaveritt@gmail.com>
2023-01-02 11:38:29 -0600

    fix: organized code

--- cashregister.js
+++ cashregister.js
@@ -11,11 +11,11 @@ let moneyVal = [
 ];
   
   
- function checkCashRegister(price, cash, cid) {
+ function checkCashRegister(price, cash, drawer) {
  
  let output = {status: null, change: []};
  let change = cash - price;
- let register = cid.reduce(function(acc, curr) {
+ let register = drawer.reduce(function(acc, curr) {
  acc.total += curr[1];
  acc[curr[0]] = curr[1];
  return acc;
@@ -23,7 +23,7 @@ let moneyVal = [
  
  if(register.total === change) {
  output.status = 'CLOSED';
- output.change = cid;
+ output.change = drawer;
  return output;
  }
  
@@ -56,6 +56,3 @@ let moneyVal = [
  output.change = change_arr;
  return output;
 }
-
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
